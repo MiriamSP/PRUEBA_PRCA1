de7c6997ad57c38e43f218e636405011a9841a59
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-09-29 23:28:24 +0200

    c31Test (faltaban comillas 31)

--- spai2/src/main/java/es/upm/miw/C31.java
+++ spai2/src/main/java/es/upm/miw/C31.java
@@ -2,9 +2,9 @@ package es.upm.miw;
 
 public class C31 {
     public String m1(){
-        return ("m2")
+        return ("m2");
     }
     public String m2(){
-        return ("m2")
+        return ("m2");
     }
 }
